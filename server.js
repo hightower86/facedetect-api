91ce7f5bb26eb747ba5d9eeedac9050d72c9e3cb
Adel <htwr86@gmail.com>
2019-08-22 21:55:37 +0300

    286.5 register in controllers

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcrypt');
 const cors = require('cors');
 const knex = require('knex');
 
+const handleRegister = require('./controllers/register');
+
 const app = express();
 
 app.use(cors());
@@ -19,36 +21,9 @@ const db = knex({
   }
 });
 
-const dataBase = {
-  users: [{
-      id: "123",
-      name: 'john',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: "124",
-      name: 'sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: "125",
-      name: 'bob',
-      email: 'bob@gmail.com',
-      password: 'cherry',
-      entries: 0,
-      joined: new Date()
-    },
-  ]
-};
 
 app.get('/', (req, res) => {
-  res.send(dataBase.users);
+  res.json('what are you looking for?');
 });
 
 app.post('/signin', (req, res) => {
@@ -70,40 +45,7 @@ app.post('/signin', (req, res) => {
 
 });
 
-app.post('/register', (req, res) => {
-
-  const {
-    name,
-    email,
-    password
-  } = req.body;
-
-  const hash = bcrypt.hashSync(password, 10);
-
-  db.transaction(trx => {
-      trx.insert({
-          hash: hash,
-          email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-          return trx('users')
-            .returning('*')
-            .insert({
-              name: name,
-              email: loginEmail[0],
-              joined: new Date()
-            })
-            .then(user => {
-              res.json(user[0]);
-            });
-        })
-        .then(trx.commit)
-        .catch(trx.rollback);
-    })
-    .catch(err => res.status(400).json('unable to register'));
-});
+app.post('/register', (req, res) => handleRegister(db, bcrypt, req, res));
 
 app.get('/profile/:id', (req, res) => {
 
